368a32d92ff7fa9af4f01d23562da24ca8e96225
luongdangvp2000 <luongdangvp2000@gmail.com>
2022-09-07 18:22:25 +0000

    add sths

--- models/original_gan.py
+++ models/original_gan.py
@@ -6,12 +6,20 @@ from torch.utils.data import DataLoader
 
 
 class Generator(nn.Module):
+    """
+    Generative Adversarial Network Generator Class
+    Takes in a latent vector z and returns a vector in
+    the same image space that the discriminator is trained on.
+    """
+
     def __init__(self, img_size, latent_size, hidden_size):
-        super().__init__()
+        super(Generator, self).__init__()
+
         self.latent_size = latent_size
+
         self.model = nn.Sequential(
             nn.Linear(latent_size, hidden_size),
-            nn.ReLU,
+            nn.ReLU(),
             nn.Dropout(),
             nn.Linear(hidden_size, hidden_size),
             nn.ReLU(),
@@ -21,10 +29,23 @@ class Generator(nn.Module):
         )
 
     def forward(self, z):
+        """
+        Forward pass of a generator
+        :param z: Latent space vector - size: batch_size x latent_size
+        :return: Tensor of self.img_size
+        """
         return self.model(z)
 
 
+
+
 class Discriminator(nn.Module):
+    """
+    Generative Adverserial Network Discriminator Class
+    Takes in an image as input and outputs a probability indicating whether or not
+    the input belongs to a the real data distribution.
+    """
+
     def __init__(self, img_size, hidden_size):
         super(Discriminator, self).__init__()
 
@@ -32,15 +53,21 @@ class Discriminator(nn.Module):
 
         self.model = nn.Sequential(
             nn.Linear(img_size, hidden_size),
-            nn.LeakyReLu(),
+            nn.LeakyReLU(),
             nn.Dropout(),
             nn.Linear(hidden_size, hidden_size),
-            nn.LeakyReLu(),
+            nn.LeakyReLU(),
             nn.Dropout(),
             nn.Linear(hidden_size, 1),
             nn.Sigmoid()
         )
 
     def forward(self, x):
+        """
+        Forward pass of a discriminator
+        :param x: Image tensor
+        :return: Float in range [0, 1] - probability score
+        """
+        # Resize x from a H x W img to a vector
         x = x.view(-1, self.img_size)
         return self.model(x).clamp(1e-9)
