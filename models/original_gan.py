5d97b092ee48b3eedb5ad9166d1b97e334395585
luongdangvp2000 <luongdangvp2000@gmail.com>
2022-09-07 17:53:50 +0000

    change model

--- models/original_gan.py
+++ models/original_gan.py
@@ -6,16 +6,41 @@ from torch.utils.data import DataLoader
 
 
 class Generator(nn.Module):
-    def __init__(self):
+    def __init__(self, img_size, latent_size, hidden_size):
         super().__init__()
+        self.latent_size = latent_size
+        self.model = nn.Sequential(
+            nn.Linear(latent_size, hidden_size),
+            nn.ReLU,
+            nn.Dropout(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(),
+            nn.Linear(hidden_size, img_size),
+            nn.Tanh()
+        )
 
-        def block(in_features, out_features, normalize=True):
-            layers = [nn.Linear(in_features, out_features)]
-            if normalize:
-                layers.append(nn.BatchNorm1d(out_features, 0.8))
-            layers.append(nn.LeakyReLU(0.2, inplace=True))
-            return layers
+    def forward(self, z):
+        return self.model(z)
+
+
+class Discriminator(nn.Module):
+    def __init__(self, img_size, hidden_size):
+        super(Discriminator, self).__init__()
+
+        self.img_size = img_size
 
         self.model = nn.Sequential(
-            
+            nn.Linear(img_size, hidden_size),
+            nn.LeakyReLu(),
+            nn.Dropout(),
+            nn.Linear(hidden_size, hidden_size),
+            nn.LeakyReLu(),
+            nn.Dropout(),
+            nn.Linear(hidden_size, 1),
+            nn.Sigmoid()
         )
+
+    def forward(self, x):
+        x = x.view(-1, self.img_size)
+        return self.model(x).clamp(1e-9)
